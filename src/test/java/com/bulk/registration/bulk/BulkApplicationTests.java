ed2d7b9c25dd0be1725e27ec5a8982a27b6fc2e3
Ryu_woong <jin02014@naver.com>
2024-06-10 19:04:50 +0900

    프로젝트생성

---
+++ src/test/java/com/bulk/registration/bulk/BulkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulk.registration.bulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
